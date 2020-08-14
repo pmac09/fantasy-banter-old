d88963393e0e93421a6a84e3556bb94e102b5297
Paul McGrath <paul.mcgrath@outlook.com>
2020-08-14 18:47:49 +1000

    Updated refresh to get only most recent rounds

--- references/functions/refresh_sc_data.R
+++ references/functions/refresh_sc_data.R
@@ -1,23 +1,31 @@
-refresh_sc_data <- 
-  
-  function(cid, tkn){
+refresh_sc_data <- function(cid, tkn, rnd = NULL){
   
   path <- paste0('./references/data/raw/')
   
   auth_headers <- get_sc_auth(cid, tkn)
   settings <- get_sc_settings(auth_headers)
-  rnd <- settings$competition$current_round
   
-  for(i in 1:rnd){
-    
-    message(paste0('Downloading round ', i ,' data...'))
-    
-    league_data <- get_sc_league_raw(auth_headers, i)
-    saveRDS(league_data, file=paste0(path, sprintf("2020_%02d", i), '_SC_LEAGUE_DATA.RDS'))
-
-    player_data <- get_sc_player_raw(auth_headers, i)
-    saveRDS(player_data, file=paste0(path, sprintf("2020_%02d", i), '_SC_PLAYER_DATA.RDS'))
-    
+  if(is.null(rnd)){
+    rnd <- settings$competition$current_round
   }
+  
+  # Download selected round details
+  message(paste0('Downloading round ', rnd ,' data...'))
+  
+  league_data <- get_sc_league_raw(auth_headers, rnd)
+  saveRDS(league_data, file=paste0(path, sprintf("2020_%02d", rnd), '_SC_LEAGUE_DATA.RDS'))
+  
+  player_data <- get_sc_player_raw(auth_headers, rnd)
+  saveRDS(player_data, file=paste0(path, sprintf("2020_%02d", rnd), '_SC_PLAYER_DATA.RDS'))
+  
+  # Download live round details
+  nxt_rnd <- settings$competition$next_round
+  message(paste0('Downloading round ', nxt_rnd,'(live) data...'))
+  
+  league_data <- get_sc_league_raw(auth_headers, nxt_rnd)
+  saveRDS(league_data, file=paste0(path, '2020_LIVE_SC_LEAGUE_DATA.RDS'))
+  
+  player_data <- get_sc_player_raw(auth_headers, nxt_rnd)
+  saveRDS(player_data, file=paste0(path, '2020_LIVE_SC_PLAYER_DATA.RDS'))
   
 }
